6e5b37d4ba7b743c3a0c3b0c57f549eec8efd0fb
vitorotto <vitorotto05@gmail.com>
2025-04-10 12:28:01 -0300

    Inicio do ex012 - números primos

---
+++ aula04-repeticoes/exercicios/ex012.c
@@ -0,0 +1,14 @@
+// Escreva um programa que leia um número e diga se ele é primo ou não.
+
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    system("cls");
+
+    
+
+    return 0;
+}
