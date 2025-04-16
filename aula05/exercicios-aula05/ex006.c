0944d079b1e9beb3434572e51270d09acb07aaad
vitorotto <vitorotto05@gmail.com>
2025-04-15 23:26:38 -0300

    Começo ex006

---
+++ aula05/exercicios-aula05/ex006.c
@@ -0,0 +1,18 @@
+// Escreva um programa que leia 10 números inteiros e os armazene em um vetor. Imprima o vetor, o maior elemento e a posição que ele se encontra.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    system("cls");
+
+    int tam = 5;
+    int arr[tam];
+
+    for (int i = 0; i < tam; i++) {
+        printf("Insira o valor %d: ");
+        // scanf("%d")
+    }
+}
