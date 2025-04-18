37096c055e4cb59f60d3ef75e49e4f3cb0584e0a
vitorotto <vitorotto05@gmail.com>
2025-04-18 15:08:55 -0300

    Ex006 concluído

--- aula05/exercicios-aula05/ex006.c
+++ aula05/exercicios-aula05/ex006.c
@@ -8,11 +8,30 @@ int main() {
     SetConsoleOutputCP(65001);
     system("cls");
 
-    int tam = 5;
+    int tam = 5, maior, posMaior;
     int arr[tam];
 
     for (int i = 0; i < tam; i++) {
-        printf("Insira o valor %d: ");
-        // scanf("%d")
+        printf("Insira o valor %d: ", i);
+        scanf("%d", &arr[i]);
+
+        // maior
+        if (i == 0) {
+            maior = arr[i];
+            posMaior = i;
+        }
+
+        if (arr[i] > maior) {
+            maior = arr[i];
+            posMaior = i;
+        }
     }
+
+    printf("Vetor: ");
+    for (int i = 0; i < tam; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nMaior: %d\n", maior);
+    printf("Posição: %d", posMaior);
 }
