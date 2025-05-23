5a82a69fa3b4b1531dd4c5d73a16b3d27e93d0e3
vitorotto <vitorotto05@gmail.com>
2025-05-22 23:01:55 -0300

    ATT: ex003 de loops concluído

--- aula09/revisao/loops/ex003.c
+++ aula09/revisao/loops/ex003.c
@@ -3,9 +3,27 @@
 #include<stdio.h>
 #include<windows.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main() {
         SetConsoleOutputCP(65001);
         system("cls");
+        srand(time(NULL));
+
+        int val = rand()%100;
+        int palpite;
+
+        do {
+            printf("Adivinhe: ");
+            scanf("%d", &palpite);
+            if (palpite < val) {
+                printf("Quase! Seu palpite está abaixo do valor.\n");
+            } else if (palpite > val) {
+                printf("Quase! Seu palpite está acima do valor.\n");
+            } else {
+                printf("Boaa!!! Acertou - valor: %d \n", val);
+            }
+        } while (palpite != val);
+
     return 0; 
 }
