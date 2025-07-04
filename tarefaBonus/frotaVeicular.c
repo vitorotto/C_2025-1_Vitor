4ba717ba36e20972c8f03fcc3e915b2d1653be0c
vitorotto <vitorotto05@gmail.com>
2025-07-03 23:45:27 -0300

    Final: tarefa bônus concluída e funcional

--- tarefaBonus/frotaVeicular.c
+++ tarefaBonus/frotaVeicular.c
@@ -1,10 +1,3 @@
-/*Modelo (string)
-Placa (string)
-Quilometragem total (kmTotal): valor acumulado de quilômetros rodados
-Quilometragem desde a última revisão (kmRevisao): deve ser zerado sempre que o veículo passar por uma revisão
-Status do veículo (emUso): um booleano que indica se o veículo está fora da garagem ou disponível na garagem*/
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -22,19 +15,34 @@ void menu() {
     printf("Menu:\n");
     printf("1. Cadastrar veículo\n");
     printf("2. Listar veículos\n");
-    printf("3. Atualizar quilometragem\n");
-    printf("4. Realizar revisão\n");
-    printf("5. Sair\n");
-    printf("Escolha uma opção: ");
+    printf("3. Registrar saída\n");
+    printf("4. Registrar entrada\n");
+    printf("5. Realizar revisão\n");
+    printf("6. Sair\n");
+    printf("\nEscolha uma opção: ");
+}
+int verificaSeListaVazia(int count) {
+    return count == 0; // Retorna 1 se a lista estiver vazia
+}
+
+int verificaSeTodosEmUso(Carro *frota, int count) {
+    for (int i = 0; i < count; i++) {
+        if (frota[i].emUso == 'N') {
+            return 0; // Existe algum veículo disponível
+        }
+    }
+    return 1; // Todos os veículos estão em uso
 }
 
 int main() {
+    system("clear");
     menu();
     Carro *frota = NULL;
     int opcao;
     int count = 0;
     do {
         scanf("%d", &opcao);
+
         switch(opcao) {
             case 1: // Cadastrar veículo
                 frota = realloc(frota, (count + 1) * sizeof(Carro));
@@ -48,62 +56,158 @@ int main() {
                 scanf("%s", frota[count].placa);
                 printf("Digite a quilometragem do veículo: ");
                 scanf("%f", &frota[count].KmTotal);
-                frota[count].KmRevisao = 0; // Inicialmente, quilometragem desde a última revisão é zero
-                frota[count].emUso = 'N'; // Inicialmente não está em uso
+                frota[count].KmRevisao = 0;
+                frota[count].emUso = 'N';
                 count++;
+
+                printf("\nVeículo cadastrado com sucesso.\n");
+
                 break;
             case 2: // Listar veículos
+
+                if (verificaSeListaVazia(count)) {
+                    printf("\nNenhum veículo cadastrado.\n");
+                    break;
+                }
+
+                printf("\nLista de veículos:\n");
                 for (int i = 0; i < count; i++) {
                     printf("Modelo: %s, Placa: %s, Km Total: %.2f, Km Revisão: %d, Em Uso: %c\n",
                            frota[i].modelo, frota[i].placa, frota[i].KmTotal, 
                            frota[i].KmRevisao, frota[i].emUso);
                 }
                 break;
-            case 3: // Atualizar quilometragem
-                printf("Digite a placa do veículo para atualizar a quilometragem: ");
+            case 3: // Registrar saída
+                printf("\n");
+                printf("Registrar saída de veículo:\n");
+
+                if (verificaSeListaVazia(count)) {
+                    printf("\nNenhum veículo cadastrado.\n");
+                    break;
+                }
+
+                // Verificando se todos os veículos estão em uso
+                if (verificaSeTodosEmUso(frota, count)) {
+                    printf("\nTodos os veículos estão em uso. Não é possível realizar saída.\n");
+                    break;
+                }
+
+                // Veiculos na garagem
+                printf("\nVeículos na garagem:\n");
+                for (int i = 0; i < count; i++) {
+                    if (frota[i].emUso == 'N') {
+                        printf("Modelo: %s, Placa: %s, Km Total: %.2f, Km Revisão: %d\n",
+                               frota[i].modelo, frota[i].placa, 
+                               frota[i].KmTotal, frota[i].KmRevisao);
+                    }
+                }
+                // Registrar saída
+                printf("\nDigite a placa do veículo para registrar a saída: ");
                 char placa[10];
                 scanf("%s", placa);
                 for (int i = 0; i < count; i++) {
                     if (strcmp(frota[i].placa, placa) == 0) {
-                        float kmAtualizada;
-                        printf("Digite a nova quilometragem: ");
-                        scanf("%f", &kmAtualizada);
-                        frota[i].KmTotal += kmAtualizada; // Atualiza a quilometragem total
-                        frota[i].KmRevisao += kmAtualizada; // Atualiza a quilometragem desde a última revisão
-                        printf("Quilometragem atualizada com sucesso.\n");
+                        if (frota[i].emUso == 'N') {
+                            if (frota[i].KmRevisao >= 10000) { // Verifica se o veículo precisa de revisão
+                                printf("\nO veículo precisa passar por revisão antes de sair.\n");
+                                break;
+                            } else {
+                                frota[i].emUso = 'S';
+                                printf("\nSaída registrada com sucesso.\n");
+                            }
+                        } else {
+                            printf("\nO veículo já está fora da garagem.\n");
+                        }
                         break;
                     }
                 }
-                // exibe o veículo atualizado
+                break;
+            case 4: // Registrar entrada
+                printf("\n");
+                printf("Registrar entrada de veículo:\n");
+
+                if (verificaSeListaVazia(count)) {
+                    printf("\nNenhum veículo cadastrado.\n");
+                    break;
+                }
+
+                // Verificando se não existe nenhum veículo fora da garagem
+                if (!verificaSeTodosEmUso(frota, count)) {
+                    printf("\nTodos estão na garagem. Não é possível registrar a entrada.\n");
+                    break;
+                }
+
+                // Veiculos fora da garagem
+                printf("\nVeículos fora da garagem:\n");
+                for (int i = 0; i < count; i++) {
+                    if (frota[i].emUso == 'S') {
+                        printf("Modelo: %s, Placa: %s, Km Total: %.2f, Km Revisão: %d\n",
+                               frota[i].modelo, frota[i].placa, 
+                               frota[i].KmTotal, frota[i].KmRevisao);
+                    }
+                }
+                // Registrar entrada
+                printf("\nDigite a placa do veículo para registrar a entrada: ");
+                scanf("%s", placa);
+                printf("Digite a quilometragem rodada do veículo: ");
+                float kmRodada;
+                scanf("%f", &kmRodada);
                 for (int i = 0; i < count; i++) {
                     if (strcmp(frota[i].placa, placa) == 0) {
-                        printf("Veículo atualizado: Modelo: %s, Placa: %s, Km Total: %.2f, Km Revisão: %d, Em Uso: %c\n",
-                               frota[i].modelo, frota[i].placa, frota[i].KmTotal, 
-                               frota[i].KmRevisao, frota[i].emUso); 
+                        if (frota[i].emUso == 'S') {
+                            frota[i].KmTotal += kmRodada; // Atualiza a quilometragem total
+                            frota[i].KmRevisao += frota[i].KmTotal; // Atualiza a quilometragem desde a última revisão
+                            frota[i].emUso = 'N';
+                            printf("\nEntrada registrada com sucesso.\n");
+                        } else {
+                            printf("\nO veículo já está na garagem.\n");
+                        }
+                        break;
+                    }
+                }
                 break;
-            case 4: // Realizar revisão
+            case 5: // Realizar revisão
+                printf("\n");
+                printf("Realizar revisão de veículo:\n");
+
+                if (verificaSeListaVazia(count)) {
+                    printf("\nNenhum veículo cadastrado.\n");
+                    break;
+                }
+
+                // Verificando se todos os veículos estão em uso
+                if (verificaSeTodosEmUso(frota, count)) {
+                    printf("\nTodos os veículos estão em uso. Não é possível realizar revisão.\n");
+                    break;
+                }
                 printf("Digite a placa do veículo para realizar revisão: ");
                 scanf("%s", placa);
                 for (int i = 0; i < count; i++) {
-                    if (strcmp(frota[i].placa, placa) == 0) {
-                        frota[i].KmRevisao = 0; // Zera a quilometragem desde a última revisão
-                        printf("Revisão realizada com sucesso.\n");
+                    if (strcmp(frota[i].placa, placa) == 0 && frota[i].emUso == 'N') {
+                        frota[i].KmRevisao = 0;
+                        printf("\nRevisão realizada com sucesso.\n");
                         break;
+                    } else {
+                        if (strcmp(frota[i].placa, placa) == 0 && frota[i].emUso == 'S') {
+                            printf("\nO veículo está em uso. Não é possível realizar a revisão.\n");
+                            break;
+                        }
                     }
                 }
                 break;
-            case 5: // Sair
-                printf("Saindo do programa.\n");
-                free(frota); // Libera a memória alocada
+            case 6: // Sair
+                printf("\nSaindo do programa.\n");
+                free(frota);
                 break;
             default:
-                printf("Opção inválida. Tente novamente.\n");
+                printf("\nOpção inválida. Tente novamente.\n");
                 break;
-            }
         }
-    }
-    menu(); // Exibe o menu novamente após cada ação
-    } while (opcao != 5);
+        if (opcao != 6) {
+            printf("\n");
+        }
+    menu(); // Exibe o menu denovo
+    } while (opcao != 6);
     
     return 0;
 }
